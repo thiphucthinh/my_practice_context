7d38bb58e29eda2fb8f6169f5dacfd87bc71d1ad
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-09-12 15:02:02 -0700

    Problem 08 to 11 Solutions

--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,4 +1,27 @@
-// Your code here
+class CallCenter {
+	constructor(name) {
+		this.name = name;
+	}
+
+	sayHello() {
+		console.log(`Hello this is ${this.name}`);
+	}
+
+	callMeLater(delay) {
+		setTimeout(() => this.sayHello(), delay);
+	}
+}
+
+
+// Example 1
+let judy = new CallCenter("Judy");
+console.log(judy.sayHello());         // prints "Hello this is Judy"
+console.log(judy.callMeLater(1000));  // waits one second then prints "Hello this is Judy"
+
+// Example 2
+let melan = new CallCenter("Melan");
+console.log(melan.sayHello());        // prints "Hello this is Melan"
+console.log(melan.callMeLater(1000)); // waits one second then prints "Hello this is Melan"
 
 
 /*****************************************************************************/
--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,4 +1,43 @@
-// Your code here
+class PartyPlanner {
+	constructor(guestList) {
+		this.guestList = [];
+	}
+
+	addToGuestList(name) {
+		this.guestList.push(name);
+	}
+
+	throwParty() {
+		if (this.guestList.length === 0) {
+			return "Gotta add people to the guest list";
+		}
+		else {
+			return `Welcome to the party ${this.guestList.join(" and ")}`;
+			// The party.throwParty() maintains the guests name over the time
+			// because the party.throwParty() is capturing and maintaining a reference
+			// to the guestList array from the PartyPlanner instance when it is created
+		}
+	}
+
+}
+
+// Example 1
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
+
+// Example 2
+const party2 = new PartyPlanner();
+console.log(party2.throwParty()); // prints "Gotta add people to the guest list"
+
+party2.addToGuestList("Lucy");
+console.log(party2.throwParty()); // prints "Welcome to the party Lucy"
 
 
 /*****************************************************************************/
